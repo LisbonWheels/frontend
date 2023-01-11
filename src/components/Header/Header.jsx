5cce0fd9c3f577a72678be1774aa0f489f1faf0f
Andreas Leoncedis <info.andreasleoncedis@gmail.com>
2023-01-11 16:53:12 +0000

    header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="Header-text">
+        <h2>Lisbon Rental Cars</h2>
+        <p>Choose your new ways of transportation now !</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
