902aeae585dd48ed69ef7ed03913eae4cf948d1f
Carla Gonçalves <goncalves.carla1998@gmail.com>
2023-01-11 11:51:41 +0000

    Initial setup to the frontend development

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import CarDetails from "./pages/CarDetails/CarDetails";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/car-details" element={<CarDetails />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
