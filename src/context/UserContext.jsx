b4cd96de43eb727b045e556a4cc78836d616ecbc
Orenikoe <orental21@gmail.com>
2023-01-13 09:22:27 +0000

    fixed login bug

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -9,11 +9,15 @@ export default UserContext;
 
 export function UserContextProvider({ children }) {
   const [userDetails, setUserDetails] = useState(null);
+  const [username, setUsername] = useState(localStorage.getItem("CognitoIdentityServiceProvider.7pb8nac8533lbhkikqn27dmpge.LastAuthUser"));
   const [userNotFound, setUserNotFound] = useState(false);
+  const getUserJwt = localStorage.getItem('CognitoIdentityServiceProvider.7pb8nac8533lbhkikqn27dmpge.' + username + '.accessToken')
+  console.log(getUserJwt)
   
   useEffect(() => {
-      if (localStorage.getItem('CognitoIdentityServiceProvider.7pb8nac8533lbhkikqn27dmpge.10ff4fdd-f5b3-4f6c-9a46-6e19c29be8fe.accessToken') !== null) {
-        let parsed_token = jwt_decode(localStorage.getItem('CognitoIdentityServiceProvider.7pb8nac8533lbhkikqn27dmpge.10ff4fdd-f5b3-4f6c-9a46-6e19c29be8fe.accessToken'));
+    console.log(username)
+      if (localStorage.getItem(`CognitoIdentityServiceProvider.7pb8nac8533lbhkikqn27dmpge.${username}.accessToken`) !== null) {
+        let parsed_token = jwt_decode(localStorage.getItem(`CognitoIdentityServiceProvider.7pb8nac8533lbhkikqn27dmpge.${username}.accessToken`));
         console.log(parsed_token)
 
         setUserDetails({
